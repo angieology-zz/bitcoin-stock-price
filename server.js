15ab2c96c901a5d43ccce977871e0f26bd06755e
Angela Lee <angelalee.one@gmail.com>
2017-12-23 15:38:13 -0500

    update remote with local

--- server.js
+++ server.js
@@ -5,7 +5,6 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
-//var Comment = require('./model/comments');
 var Ticker = require('./model/tickers');
 
 //and create our instances
